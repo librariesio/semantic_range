a88d2f40a276fa5db377d55f11eb5d42c2db9a04
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-12 18:47:30 +0000

    Working comparator class

--- lib/semantic_range/comparator.rb
+++ lib/semantic_range/comparator.rb
@@ -1,11 +1,23 @@
 module SemanticRange
   class Comparator
     def initialize(comp, loose)
-      @comp = comp
       @loose = loose
-      @value
-      @operator
-      @semver
+
+      if comp.is_a?(Comparator)
+        if comp.loose == loose
+          return comp
+        else
+          @comp = comp.value
+        end
+      end
+
+      parse(comp)
+
+      if @semver == ANY
+        @value = ''
+      else
+        @value = @operator + @semver.version
+      end
     end
 
     def to_s
@@ -13,11 +25,50 @@ module SemanticRange
     end
 
     def test(version)
-      # TODO
+      return true if @semver == ANY
+      version = Version.new(version, @loose) if version.is_a?(String)
+      cmp(version, @operator, @semver, @loose)
     end
 
     def parse(comp)
-      # TODO
+      m = comp.match(@loose ? COMPARATORLOOSE : COMPARATOR)
+      raise 'Invalid comparator: ' + comp unless m
+
+      @operator = m[1]
+      @operator = '' if @operator == '='
+
+      if !m[2]
+        @semver = ANY
+      else
+        @semver = Version.new(m[2], @loose)
+      end
+    end
+
+    def cmp(a, op, b, loose)
+      case op
+      when '==='
+        a = a.version if !a.is_a?(String)
+        b = b.version if !b.is_a?(String)
+        a == b
+      when '!=='
+        a = a.version if !a.is_a?(String)
+        b = b.version if !b.is_a?(String)
+        a != b
+      when '', '=', '=='
+        SemanticRange.eq(a, b, loose)
+      when '!='
+        SemanticRange.neq(a, b, loose)
+      when '>'
+        SemanticRange.gt(a, b, loose)
+      when '>='
+        SemanticRange.gte(a, b, loose)
+      when '<'
+        SemanticRange.lt(a, b, loose)
+      when '<='
+        SemanticRange.lte(a, b, loose)
+      else
+        raise 'Invalid operator: ' + op
+      end
     end
   end
 end
